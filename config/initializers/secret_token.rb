b108faaa4ea7b10a545a601dd3e3a642919f48fe
Grzegorz Bunia <xenon2185@gmail.com>
2013-09-05 18:17:31 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Surebets::Application.config.secret_key_base = '7175b8841318264f6d5942961730df032ec88275d0fd351faad59ed78bb7bb2cf0922f8e51268fa238f7143474c60f68d63a432393e6100725c052eb44e60327'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Surebets::Application.config.session_store :cookie_store, key: '_surebets_session'
